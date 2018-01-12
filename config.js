aae4fc6927d51d11f3c4a17bb0bfed7845a5dcce
Alvaro <alvaro.gs86@gmail.com>
2018-01-12 10:54:17 +0000

    Config to heroku deploy

--- config.js
+++ config.js
@@ -2,7 +2,8 @@ var config = {
     spotify: {
         clientID: "a623cee681db4d9eb3cbade6e1bae409",
         clientSecret: "c0fbfbbd083b49c7a27a8ceaf324d9d4",
-        callbackURI: "https://spoty-list-mix-alvaro07.c9users.io/callback/" 
+        // callbackURI: "https://spoty-list-mix-alvaro07.c9users.io/callback/"
+        callbackURI: "https://spoty-list.herokuapp.com/callback/" 
     }
 };
 
