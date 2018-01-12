9e851365747d949dc3c94ad17a03d93d97485b68
Alvaro <alvaro.gs86@gmail.com>
2018-01-12 11:01:03 +0000

    fix bug in config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ var config = {
         clientID: "a623cee681db4d9eb3cbade6e1bae409",
         clientSecret: "c0fbfbbd083b49c7a27a8ceaf324d9d4",
         // callbackURI: "https://spoty-list-mix-alvaro07.c9users.io/callback/"
-        callbackURI: "https://spoty-list.herokuapp.com/callback/" 
+        callbackURI: "https://spoty-list-mix.herokuapp.com/callback/" 
     }
 };
 
