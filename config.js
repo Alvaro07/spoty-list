830fec22c2865ad1166e1e82c554b9a631a09b2c
Alvaro <alvaro.gs86@gmail.com>
2018-02-20 07:54:10 +0000

    changes in readme

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ var config = {
         clientID: "103d44f8ecc94d6a8257c36c328ae001",
         // clientID: "e125250182e145f2bab7113e4314538c",
         
-        clientSecret: "980c08fa889b48cd8c70b3ee0698e18c ",
+        clientSecret: "980c08fa889b48cd8c70b3ee0698e18c",
         // clientSecret: "42a10d393c8d441e9c9ea1ca8c55f335",
         
         callbackURI: "https://spoty-list.herokuapp.com/callback/" 
