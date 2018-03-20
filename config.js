27274c3886de6678f9bd692d56913fd71b23ffec
Alvaro <alvaro.gs86@gmail.com>
2018-03-20 15:38:32 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 var config = {
     spotify: {
-        clientID: "103d44f8ecc94d6a8257c36c328ae001",
-        // clientID: "e125250182e145f2bab7113e4314538c",
+        clientID: "",
+        // clientID: "",
         
-        clientSecret: "980c08fa889b48cd8c70b3ee0698e18c",
-        // clientSecret: "42a10d393c8d441e9c9ea1ca8c55f335",
+        clientSecret: "",
+        // clientSecret: "",
         
         callbackURI: "https://spoty-list.herokuapp.com/callback/" 
         // callbackURI: "https://spoty-list-mix-alvaro07.c9users.io/callback/"
